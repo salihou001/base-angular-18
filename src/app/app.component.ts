9f80a9bed8cf5877feb35103ebd562eb4342c568
aboubakar-salihou <aboubakar.salihou@londo-tech.com>
2024-06-03 15:03:47 +0100

    adding splitting librairie

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import  Splitting  from 'splitting';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import gsap from 'gsap';
+import Lenis from '@studio-freight/lenis';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +11,39 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'angular-18';
+export class AppComponent implements OnInit {
+
+  ngOnInit(): void {
+    Splitting();
+    const lenis = new Lenis()
+    lenis.on('scroll', (e:any) => {
+    })
+    lenis.on('scroll', ScrollTrigger.update)
+    gsap.ticker.add((time) => {
+      lenis.raf(time * 1000)
+    })
+    gsap.ticker.lagSmoothing(0)
+  }
+
+  moving(e: any) {
+    gsap.to('.hand', {
+      left: e.clientX - 50 + 'px',
+      top: e.clientY - 50 + 'px',
+      // delay: .1,
+      ease: 'sine.out'
+    })
+  }
+  enter(e: any) {
+    gsap.fromTo(".hand", { scale: 0 }, {
+      scale: 1,
+      opacity: 1,
+    })
+  }
+
+  leave(e: any) {
+    gsap.to(".hand", {
+      scale: 0,
+      opacity: 0
+    })
+  }
 }
